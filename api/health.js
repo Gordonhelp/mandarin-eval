262ba255ed82f641d4b487357bed4e57d158e5f6
Gordonhelp <2469748059@qq.com>
2026-08-11 13:53:32 +0800

    create api/health.js

---
+++ api/health.js
@@ -0,0 +1,7 @@
+/**
+ * Vercel Serverless Function: GET /api/health
+ */
+module.exports = function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({ status: 'ok', timestamp: Date.now() });
+};
